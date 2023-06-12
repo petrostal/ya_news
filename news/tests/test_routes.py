70f8ab3c4f3979c547e9d70aea7d1e62fa103fe9
petrostal <topchienko.peter@gmail.com>
2023-06-12 22:01:55 +0300

    2/5 done

---
+++ news/tests/test_routes.py
@@ -0,0 +1,15 @@
+# news/tests/test_routes.py
+from django.test import TestCase
+from http import HTTPStatus
+from django.urls import reverse
+
+
+class TestRoutes(TestCase):
+
+    def test_home_page(self):
+        # Вместо прямого указания адреса 
+        # получаем его при помощи функции reverse().
+        url = reverse('news:home')
+        response = self.client.get(url)
+        # Проверяем, что код ответа равен статусу OK (он же 200).
+        self.assertEqual(response.status_code, HTTPStatus.OK) 
