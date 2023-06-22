4457401f3b56dea7fadd44847c33a011eae619af
petrostal <topchienko.peter@gmail.com>
2023-06-22 22:26:20 +0300

    finish

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,4 +1,3 @@
-# news/tests/test_routes.py
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
@@ -14,10 +13,8 @@ class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.news = News.objects.create(title='Заголовок', text='Текст')
-        # Создаём двух пользователей с разными именами:
         cls.author = User.objects.create(username='Лев Толстой')
         cls.reader = User.objects.create(username='Читатель простой')
-        # От имени одного пользователя создаём комментарий к новости:
         cls.comment = Comment.objects.create(
             news=cls.news, author=cls.author, text='Текст комментария'
         )
@@ -42,10 +39,7 @@ class TestRoutes(TestCase):
             (self.reader, HTTPStatus.NOT_FOUND),
         )
         for user, status in users_statuses:
-            # Логиним пользователя в клиенте:
             self.client.force_login(user)
-            # Для каждой пары "пользователь - ожидаемый ответ"
-            # перебираем имена тестируемых страниц:
             for name in ('news:edit', 'news:delete'):
                 with self.subTest(user=user, name=name):
                     url = reverse(name, args=(self.comment.id,))
@@ -53,20 +47,10 @@ class TestRoutes(TestCase):
                     self.assertEqual(response.status_code, status)
 
     def test_redirect_for_anonymous_client(self):
-        # Сохраняем адрес страницы логина:
         login_url = reverse('users:login')
-        # В цикле перебираем имена страниц, с которых ожидаем редирект:
         for name in ('news:edit', 'news:delete'):
             with self.subTest(name=name):
-                # Получаем адрес страницы редактирования или удаления
-                # комментария:
                 url = reverse(name, args=(self.comment.id,))
-                # Получаем ожидаемый адрес страницы логина,
-                # на который будет перенаправлен пользователь.
-                # Учитываем, что в адресе будет параметр next, в котором
-                # передаётся
-                # адрес страницы, с которой пользователь был переадресован.
                 redirect_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
-                # Проверяем, что редирект приведёт именно на указанную ссылку.
                 self.assertRedirects(response, redirect_url)
